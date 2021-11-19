73c4dd37c191797db86b35365a100861b706d180
Crowdbotics <team@crowdbotics.com>
2021-11-19 23:11:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "illuminati_belliger_32049",
+  url: "https://illuminati_belliger_32049.botics.co",
+  api: "https://illuminati_belliger_32049.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
